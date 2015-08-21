e84f2eccd9eac671e1431789314b7298647d24f6
James Campbell <jscampbell.05@googlemail.com>
2015-08-21 10:49:17 +0100

    Adds in task class.

---
+++ Pod/Classes/BFLYSessionTask.h
@@ -0,0 +1,13 @@
+//
+//  BFLYSessionTask.h
+//  Pods
+//
+//  Created by James Campbell on 21/08/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BFLYSessionTask : NSObject
+
+@end
---
+++ Pod/Classes/BFLYSessionTask.m
@@ -0,0 +1,13 @@
+//
+//  BFLYSessionTask.m
+//  Pods
+//
+//  Created by James Campbell on 21/08/2015.
+//
+//
+
+#import "BFLYSessionTask.h"
+
+@implementation BFLYSessionTask
+
+@end
