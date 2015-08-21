9eeaaa4d4769ea201da3eded143eb926a38a1c2b
James Campbell <jscampbell.05@googlemail.com>
2015-08-21 19:41:21 +0100

    Adds in context.

---
+++ Pod/Classes/BFLYSessionTaskContext.h
@@ -0,0 +1,13 @@
+//
+//  BFLYSessionTaskContext.h
+//  Pods
+//
+//  Created by James Campbell on 21/08/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BFLYSessionTaskContext : NSObject
+
+@end
---
+++ Pod/Classes/BFLYSessionTaskContext.m
@@ -0,0 +1,13 @@
+//
+//  BFLYSessionTaskContext.m
+//  Pods
+//
+//  Created by James Campbell on 21/08/2015.
+//
+//
+
+#import "BFLYSessionTaskContext.h"
+
+@implementation BFLYSessionTaskContext
+
+@end
